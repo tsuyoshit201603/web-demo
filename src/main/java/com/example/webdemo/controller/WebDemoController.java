cbac251cd4c2cd90990625d2afd94f1aab02fd38
tsuyoshi <tsuyoshi.t201603@gmail.com>
2022-11-02 22:47:45 +0900

    first commit

---
+++ src/main/java/com/example/webdemo/controller/WebDemoController.java
@@ -0,0 +1,13 @@
+package com.example.webdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebDemoController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "hello world!!";
+    }
+}
